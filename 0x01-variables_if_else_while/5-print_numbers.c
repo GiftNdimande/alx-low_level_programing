42c349a24e1acc29cd6f73cc3ce91f6af4f7a04c
GiftNdimande <u19063815@tuks.co.za>
2022-12-09 16:58:13 -0800

    Variables,if and else

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*main -prints all single digit number bases
+*
+*Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int n;
+
+	for (n=0;n<10;n++)
+		printf("%d", n);
+	printf("\n");
+	return (0);
+}
