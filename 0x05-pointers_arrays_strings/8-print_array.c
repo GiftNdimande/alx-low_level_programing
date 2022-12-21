504d4c6c75b51c8f38bf4a43900711bc7f8b9001
GiftNdimande <u19063815@tuks.co.za>
2022-12-20 16:56:51 -0800

    Fixed Script 8 and 9 plus Advance Task

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -5,7 +5,7 @@
  * print_array - check code
  * @a: name of array
  * @n: number of ellements
- * Return: a and n inputs 
+ * Return: a and n inputs
  */
 void print_array(int *a, int n)
 {
@@ -14,8 +14,7 @@ void print_array(int *a, int n)
 	for (i = 0; i < (n - 1); i++)
 	{
 		printf("%d, ", a[i]);
-		
-	}	
+	}
 		if (i == (n - 1))
 		{
 			printf("%d", a[n - 1]);
