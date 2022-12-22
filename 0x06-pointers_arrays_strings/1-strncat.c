1f2160cb95aa8a02e21afe36b0b0e13d3edecb49
GiftNdimande <u19063815@tuks.co.za>
2022-12-22 15:47:04 -0800

    fixed task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * _strncat - Concatenates two strings using at most an inputted number of bytes from src
+ * _strncat - Concatenates two strings using number of bytes from src
  * @dest: The string to be appended upon
  * @src: The string to be appended to
  * @n: The number of bytes from src to be appended to
